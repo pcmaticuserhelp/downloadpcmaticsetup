42078c6101635fce2e3610fc18e726aad6d97ad6
pcmaticuserhelp <elipseiromy@outlook.com>
2026-07-13 15:19:35 +0530

    Create conf.py for Sphinx documentation
    Add Sphinx configuration for pcmatic-download project.

---
+++ docs/conf.py
@@ -0,0 +1,20 @@
+project = 'pcmatic-download'
+author = 'pcmatic-download'
+release = '1.0'
+
+# Extensions
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Paths
+templates_path = ['_templates']
+exclude_patterns = []
+
+# Theme
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Custom JS & Favicon
+html_js_files = ['chatbot.js']  # chatbot widget
+html_favicon = '_static/favicon.png'
